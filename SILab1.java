943f3ead2b3f1fd2336f5d4dfee229c220e26199
Marinst9 <129936768+Marinst9@users.noreply.github.com>
2023-04-05 12:47:36 +0200

    add java class

--- SILab1.java
+++ SILab1.java
@@ -3,10 +3,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SILab1 {
-<<<<<<< HEAD
-    /*ova e master */
-=======
->>>>>>> d0bdf57830e1f5a60d737f8a48f7fc63bcc4659b
+
+  
+
     public static List<Integer> filterOddNumbers(List<Integer> list) {
         List<Integer> result = new ArrayList<>();
         for (Integer el : list) {
